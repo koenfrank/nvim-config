000ed7c3a15135df2c64c048f372f20bff33c288
Koen <koen.grotenhuis@unit040.com>
2024-04-26 15:27:17 +0200

    Added codelens refresh, and neogen

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -4,5 +4,16 @@ return {
     config = true,
     -- Uncomment next line if you want to follow only stable versions
     -- version = "*"
+    opts = {
+      enabled = true,
+      languages = {
+        cs = {
+          template = {
+            -- overwrite default doxygen for csharp
+            annotation_convention = "xmldoc",
+          },
+        },
+      },
+    },
   },
 }
