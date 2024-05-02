4528dc9a3a9e980296b47cfb84565db5aa9d179a
Koen Franklin <151389664+koenfrank@users.noreply.github.com>
2024-05-02 18:46:23 +0200

    remove markdown, remove netcoredbg add arm netcored

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.omnisharp" },
-    { import = "lazyvim.plguins.extras.lang.markdown" },
+    --{ import = "lazyvim.plguins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.vscode" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.dap.core" },
--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,33 +1,6 @@
 return {
   {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    opts = function()
-      print(vim.fn.exepath("netcoredbg"))
-      local dap = require("dap")
-      if not dap.adapters["netcoredbg"] then
-        require("dap").adapters["netcoredbg"] = {
-          type = "executable",
-          command = vim.fn.exepath("netcoredbg"),
-          args = { "--interpreter=vscode" },
-        }
-      end
-      for _, lang in ipairs({ "cs", "fsharp", "vb" }) do
-        if not dap.configurations[lang] then
-          dap.configurations[lang] = {
-            {
-              type = "netcoredbg",
-              name = "Launch file",
-              request = "launch",
-              ---@diagnostic disable-next-line: redundant-parameter
-              program = function()
-                return vim.fn.input("Path to dll: ", vim.fn.getcwd() .. "/", "file")
-              end,
-              cwd = "${workspaceFolder}",
-            },
-          }
-        end
-      end
-    end,
+    "Cliffback/netcoredbg-macOS-arm64.nvim",
+    dependencies = { "mfussenegger/nvim-dap" },
   },
 }
