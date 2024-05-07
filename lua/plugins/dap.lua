104d497462218ae2424e080345e8635d3d43a9ef
Koen Franklin <151389664+koenfrank@users.noreply.github.com>
2024-05-07 17:19:49 +0200

    finally got dap to work

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -3,4 +3,29 @@ return {
     "Cliffback/netcoredbg-macOS-arm64.nvim",
     dependencies = { "mfussenegger/nvim-dap" },
   },
+
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      local dap = require("dap")
+
+      dap.adapters.coreclr = {
+        type = "executable",
+        command = "/usr/local/netcoredbg",
+        args = { "--interpreter=vscode" },
+      }
+
+      dap.configurations.cs = {
+        {
+          type = "coreclr",
+          name = "launch - netcoredbg",
+          request = "launch",
+          program = function()
+            return vim.fn.input("Path to dll", vim.fn.getcwd() .. "/bin/Debug/", "file")
+          end,
+        },
+      }
+    end,
+  },
 }
